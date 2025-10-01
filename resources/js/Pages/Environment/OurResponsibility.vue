a7d37d36876963514a36474590e03e2c39de184d
jflores13 <chuychewie@gmail.com>
2025-10-01 01:05:03 -0600

    Refactored OurResponsibility section

--- resources/js/Pages/Environment/OurResponsibility.vue
+++ resources/js/Pages/Environment/OurResponsibility.vue
@@ -7,71 +7,66 @@
 
       <div class="flex flex-col gap-16">
         <!-- Emissions -->
-        <div class="space-y-6 flex flex-col md:flex-row gap-8">
-          <div class="w-2/5">
-            <img src="/img/responsibility/BoxIndustries-Emissions.webp" alt="Emissions" class="w-full h-full object-cover">
+        <div 
+          v-for="responsibility in responsibilities" 
+          :key="responsibility.title" 
+          class="flex flex-col md:flex-row gap-8"
+        >
+          <div class="w-full md:w-2/5">
+            <img 
+              :src="responsibility.img_src" 
+              :alt="responsibility.title" 
+              class="w-full h-full object-cover"
+            >
           </div>
-          <div class="w-3/5 space-y-6">
+          <div class="w-full md:w-3/5 space-y-6">
             <h3 class="text-2xl font-bold text-white">
-              Emissions
+              {{ responsibility.title }}
             </h3>
             <h4 class="text-box-yellow">
-              On the path to cleaner packaging operations
+              {{ responsibility.short_description }}
             </h4>
             <p class="text-white leading-relaxed">
-              At Box Industries, we're taking concrete steps to reduce our environmental footprint. Our ongoing efforts include the reuse and recycling of plastic and corrugated pallets, minimizing waste across our logistics processes. As we expand, we're preparing a dedicated area within our facility for recycling used cardboard — moving toward a more circular packaging model.
+              {{ responsibility.long_description }}
             </p>
           </div>
         </div>
 
-        <!-- Air Quality & Workspaces -->
-        <div class="space-y-6 flex flex-col md:flex-row gap-8">
-          <div class="w-2/5">
-            <img src="/img/responsibility/BoxIndustries-AirQuality.webp" alt="Air Quality & Workspaces" class="w-full h-full object-cover">
-          </div>
-          <div class="w-3/5 space-y-6">
-            <h3 class="text-2xl font-bold text-white">Air Quality & Workspaces</h3>
-            <h4 class="text-box-yellow">
-              Cleaner environments for better work and better impact
-            </h4>
-            <p class="text-white leading-relaxed">
-              We are committed to maintaining clean, safe, and well-ventilated production areas. By optimizing airflow and filtering particulate matter during the handling of materials, we reduce airborne dust and improve workplace conditions — all while ensuring that our processes meet or exceed environmental standards.
-            </p>
-          </div>
-        </div>
-
-        <!-- Energy Use -->
-        <div class="space-y-6 flex flex-col md:flex-row gap-8">
-          <div class="w-2/5">
-            <img src="/img/responsibility/BoxIndustries-EnergyUse.webp" alt="Energy Use" class="w-full h-full object-cover">
-          </div>
-          <div class="w-3/5 space-y-6">
-            <h3 class="text-2xl font-bold text-white">Energy Use</h3>
-            <h4 class="text-box-yellow">
-              Advancing step by step toward cleaner energy
-            </h4>
-            <p class="text-white leading-relaxed">
-              We are committed to maintaining clean, safe, and well-ventilated production areas. By optimizing airflow and filtering particulate matter during the handling of materials, we reduce airborne dust and improve workplace conditions — all while ensuring that our processes meet or exceed environmental standards.
-            </p>
-          </div>
-        </div>
-
-        <!-- Sustainable Products -->
-        <div class="space-y-6 flex flex-col md:flex-row gap-8">
-          <div class="w-2/5">
-            <img src="/img/responsibility/BoxIndustries-SustainableProducts.webp" alt="Sustainable Products" class="w-full h-full object-cover">
-          </div>
-          <div class="w-3/5 space-y-6">
-            <h3 class="text-2xl font-bold text-white">Sustainable Products</h3>
-            <h4 class="text-box-yellow">
-              Packaging with a second life
-            </h4>
-            <p class="text-white leading-relaxed">
-              We believe packaging should do more than protect — it should protect responsibly. That's why we prioritize materials that are recyclable, reusable, and sourced responsibly. Our recycled pallet program is already active, and future developments will expand into cardboard recovery and repurposing — turning old packaging into new opportunities.
-            </p>
-          </div>
-        </div>
       </div>
     </div>
   </section>
 </template>
+
+<script setup>
+const responsibilities = [
+  {
+    title: 'Emissions',
+    img_src: '/img/responsibility/BoxIndustries-Emissions.webp',
+    short_description: 'On the path to cleaner packaging operations',
+    long_description: "At Box Industries, we're taking concrete steps to reduce our environmental footprint. Our ongoing efforts include the reuse and recycling of plastic and corrugated pallets, minimizing waste across our logistics processes. As we expand, we're preparing a dedicated area within our facility for recycling used cardboard — moving toward a more circular packaging model.",
+  },
+  
+  {
+    title: 'Air Quality & Workspaces',
+    img_src: '/img/responsibility/BoxIndustries-AirQuality.webp',
+    short_description: 'Cleaner environments for better work and better impact',
+    long_description: "We are committed to maintaining clean, safe, and well-ventilated production areas. By optimizing airflow and filtering particulate matter during the handling of materials, we reduce airborne dust and improve workplace conditions — all while ensuring that our processes meet or exceed environmental standards.",
+  },
+  
+  {
+    title: 'Energy Use',
+    img_src: '/img/responsibility/BoxIndustries-EnergyUse.webp',
+    short_description: 'Advancing step by step toward cleaner energy',
+    long_description: "We are committed to maintaining clean, safe, and well-ventilated production areas. By optimizing airflow and filtering particulate matter during the handling of materials, we reduce airborne dust and improve workplace conditions — all while ensuring that our processes meet or exceed environmental standards.",
+  },
+  
+  {
+    title: 'Sustainable Products',
+    img_src: '/img/responsibility/BoxIndustries-SustainableProducts.webp',
+    short_description: 'Packaging with a second life',
+    long_description: "We believe packaging should do more than protect — it should protect responsibly. That's why we prioritize materials that are recyclable, reusable, and sourced responsibly. Our recycled pallet program is already active, and future developments will expand into cardboard recovery and repurposing — turning old packaging into new opportunities.",
+  },
+  
+] 
+
+</script>
